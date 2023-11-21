6b374c5331eb4bce35a019f4296cad4a4aa61c4f
Shigenori Suzuki <s.suzuki@gmail.com>
2023-11-21 18:09:50 +0900

    Update sum.test.ts
    an intentional error in 
    https://github.com/suzucir/hello-jest-ts/edit/main/src/chapter2/getting_started_jest/sum.test.ts

--- src/chapter2/getting_started_jest/sum.test.ts
+++ src/chapter2/getting_started_jest/sum.test.ts
@@ -3,5 +3,5 @@ import { sum } from './sum'
 // テストケースを定義
 test('1 + 2 equals 3', () => {
   // この例ではsum(1,2)を実行する際に結果として3が返されることを検証しています。
-  expect(sum(1, 2)).toBe(3)
+  expect(sum(1, 2)).toBe(4)
 })
